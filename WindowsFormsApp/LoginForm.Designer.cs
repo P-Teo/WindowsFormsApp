e7376dc4538cd2b9194b8ccf65bbe176d21207c0
Papă <teodora.papa@student.tuiasi.ro>
2025-12-30 22:09:30 +0200

    FINAL

--- WindowsFormsApp/LoginForm.Designer.cs
+++ WindowsFormsApp/LoginForm.Designer.cs
@@ -161,7 +161,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApp.Properties.Resources._9269766;
-            this.pictureBox1.Location = new System.Drawing.Point(127, 92);
+            this.pictureBox1.Location = new System.Drawing.Point(126, 92);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(120, 120);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
